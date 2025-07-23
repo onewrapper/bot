c55f477b15c7a026935a7688e33d1ba990acc04b
chrisdadev13 <chrisdadev13@gmail.com>
2025-07-23 11:28:29 +0200

    default cpu

--- bots/bot_pod_creator/bot_pod_creator.py
+++ bots/bot_pod_creator/bot_pod_creator.py
@@ -76,13 +76,12 @@ class BotPodCreator:
                         command=command,
                         resources=client.V1ResourceRequirements(
                             requests={
-                                "cpu": "1",
-                                "memory": "2Gi",
+                                "cpu": bot_cpu_request,
+                                "memory": os.getenv("BOT_MEMORY_REQUEST", "4Gi"),
                                 "ephemeral-storage": os.getenv("BOT_EPHEMERAL_STORAGE_REQUEST", "10Gi")
                             },
                             limits={
-                                "cpu": "4",
-                                "memory": "4Gi",
+                                "memory": os.getenv("BOT_MEMORY_LIMIT", "4Gi"),
                                 "ephemeral-storage": os.getenv("BOT_EPHEMERAL_STORAGE_LIMIT", "10Gi")
                             }
                         ),
